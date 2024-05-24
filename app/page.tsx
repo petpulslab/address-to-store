07d049c078721a73cb838d05dc00be3e13a1571a
shimmingyu <alsrb60@petpuls.net>
2024-05-24 14:09:27 +0900

    init

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect } from "react";
 import { ClipLoader } from "react-spinners";
-
 export default function Home() {
   const appStoreUrl =
     "https://apps.apple.com/tr/app/%EC%82%90%EB%BD%80-pippo-%EA%B0%95%EC%95%84%EC%A7%80-%EC%86%8C%EB%B3%80%EA%B2%80%EC%82%AC-%EA%B1%B4%EA%B0%95-%EC%B2%B4%ED%81%AC%EC%82%90%EB%BD%80/id6477411423";
@@ -12,8 +11,10 @@ export default function Home() {
   useEffect(() => {
     const userAgent = navigator.userAgent;
     if (/android/i.test(userAgent)) {
+      console.log("android");
       window.location.href = playStoreUrl;
     } else if (/iPad|iPhone|iPod/.test(userAgent)) {
+      console.log("ios");
       window.location.href = appStoreUrl;
     }
   }, []);
