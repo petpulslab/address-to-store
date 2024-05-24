984dbf4b2850ba115f4745b24b3321407980ed90
shimmingyu <alsrb60@petpuls.net>
2024-05-24 13:23:17 +0900

    init

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Pippo",
+  description: "Pippo 설치 페이지로 이동합니다.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect } from "react";
+import { ClipLoader } from "react-spinners";
+
+const appStoreUrl =
+  "https://apps.apple.com/us/app/%EC%82%90%EB%BD%80-pippo-%EA%B0%95%EC%95%84%EC%A7%80-%EC%86%8C%EB%B3%80%EA%B2%80%EC%82%AC-%EA%B1%B4%EA%B0%95-%EC%B2%B4%ED%81%AC%EC%82%90%EB%BD%80/id6477411423";
+const playStoreUrl =
+  "https://play.google.com/store/apps/details?id=net.petpulslab.pippo&hl=ko&gl=US";
+
+export default function Home() {
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    if (/android/i.test(userAgent)) {
+      window.location.href = playStoreUrl;
+    } else if (/iPad|iPhone|iPod/.test(userAgent)) {
+      window.location.href = appStoreUrl;
+    }
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-2xl font-bold mb-4">
+        삐뽀설치페이지로 이동중입니다.
+      </div>
+      <ClipLoader size={150} color="#e62f8b" />
+    </div>
+  );
+}
