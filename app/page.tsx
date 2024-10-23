99ec1fb064bcde40c6d3d44510c26dd57c3e7843
shimmingyu <alsrb60@petpuls.net>
2024-10-23 16:53:14 +0900

    check language

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ClipLoader } from "react-spinners";
 export default function Home() {
+  const [userLanguage, setUserLanguage] = useState<string>("");
+
   const appStoreUrl =
     "https://apps.apple.com/tr/app/%EC%82%90%EB%BD%80-pippo-%EA%B0%95%EC%95%84%EC%A7%80-%EC%86%8C%EB%B3%80%EA%B2%80%EC%82%AC-%EA%B1%B4%EA%B0%95-%EC%B2%B4%ED%81%AC%EC%82%90%EB%BD%80/id6477411423";
   const playStoreUrl =
     "https://play.google.com/store/apps/details?id=net.petpulslab.pippo&hl=ko&gl=US";
 
   useEffect(() => {
+    // 사용자 언어 감지
+    const detectedLanguage = navigator.language || navigator.languages[0];
+    console.log(detectedLanguage);
+    setUserLanguage(detectedLanguage);
     const userAgent = navigator.userAgent;
     if (/android/i.test(userAgent)) {
       console.log("android");
@@ -25,6 +31,7 @@ export default function Home() {
         삐뽀설치페이지로 이동중입니다.
       </div>
       <ClipLoader size={150} color="#e62f8b" />
+      {userLanguage && <div className="mt-4">감지된 언어: {userLanguage}</div>}
     </div>
   );
 }
